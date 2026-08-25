d671bf914242fd7ffc2fce0f98d06ca80318331d
Riya Raikwar <riyaraikwar60@gmail.com>
2026-08-25 15:02:25 +0000

    basics1

---
+++ basics1/test.js
@@ -0,0 +1,20 @@
+//variables : const,let,var
+
+
+const accountId = 524;
+let accountEmail = "piya@gmail.com";
+var accountPassword  = "12345";
+accountCity = "Kanpur";
+let accountStatus;
+
+
+// accountId = 678;   
+// const : assignment to cont variable generate type error
+// const values should be fixed 
+console.log(accountId);
+
+accountEmail = "piya4578@gmail.com";
+accountPassword  = "121212";
+accountCity = "Jaipur";
+
+console.table([accountEmail,accountPassword,accountCity,accountStatus]);
