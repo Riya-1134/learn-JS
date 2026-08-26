4520eb35eca1ee2124e269a702b9b6ad0b774923
Riya Raikwar <riyaraikwar60@gmail.com>
2026-08-26 17:30:53 +0000

    conversion and operations

--- basics1/conversion.js
+++ basics1/conversion.js
@@ -30,15 +30,15 @@ console.log(valueinNumber);   //  Nan
 console.log(typeof(valueinNumber)); //number 
 
 
-// summary
-// string "33" => 33
-// if "53ajnfkfn" => nan (number is datatype)
-// null => 0 
-// true or false will give 1 0r 0
+// // summary
+// // string "33" => 33
+// // if "53ajnfkfn" => nan (number is datatype)
+// // null => 0 
+// // true or false will give 1 0r 0
 
 
 
-// Bool conversion
+// // Bool conversion
 let islogged = 1;
 let isbool = Boolean(islogged);  
 console.log(isbool); //true
@@ -64,3 +64,32 @@ let isString = String(isNumber);
 console.log(isString);   //35
 console.log(typeof isString);  //STRING 
 
+
+// ********************Operations************************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+console.log("1"+2+3); //123
+console.log(1+2+"3"); // 33
+
+ console.log(+true); // 1
+
+ console.log(+"");  // 0
+
+
+//prefix and postfix
+
+let x = 6;
+const y = x++; // use and +1   x=7 y=6
+console.log(`x:${x}, y:${y}`);
+
+let a = 6;
+const b = ++a; // +1 and then use the new value a=7 b=7
+console.log(`a:${a}, b:${b}`);
+ 
+ 
