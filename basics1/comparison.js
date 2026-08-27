34f007346903d1e03871514bc028bdddd89cb3a3
Riya Raikwar <riyaraikwar60@gmail.com>
2026-08-27 21:20:00 +0000

    updates

--- basics1/comparison.js
+++ basics1/comparison.js
@@ -22,13 +22,40 @@
 
 
 
-console.log(undefined >= 0 );   //f
-console.log(undefined <= 0 );   //f
-console.log(undefined == 0 );       // f
-console.log(undefined == false);   // f
-console.log(undefined == " ");       //f
+// console.log(undefined >= 0 );   //f
+// console.log(undefined <= 0 );   //f
+// console.log(undefined == 0 );       // f
+// console.log(undefined == false);   // f
+// console.log(undefined == " ");       //f
 
 
 
 //to remember null == undefined
 //also [comparison operator treat null as 0]
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive) and heap (Referenece)
+
+let name = "Riya";
+
+let anothername = name;
+anothername = "Piya";
+
+console.log(anothername);
+
+let user1 = {
+    name: "Joy",
+    age: 23
+
+}
+
+let user2 = user1;
+
+user2.age=34;
+
+console.log(user1.age);
+console.log(user2.age);
+
+//stack all primitive datatypes goes in stack and give the copy only of values (does not change original values)
+// Heap all non primitve  goes here and refer to same values changes affect original values stored in heap
