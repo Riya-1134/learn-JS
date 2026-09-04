4087caf75c0f3a03ee57a7229cd23f3e70279e5b
Riya Raikwar <riyaraikwar60@gmail.com>
2026-09-04 22:20:38 +0000

    scope

---
+++ basics1/scope.js
@@ -0,0 +1,16 @@
+let name = "Riya";
+function a(){
+    console.log(name); // accesible
+    
+}
+
+
+
+function b(){
+
+    let name1 = "piya";
+}
+
+console.log(name1); // not accesible
+
+// scope depends on lexical enviroment
