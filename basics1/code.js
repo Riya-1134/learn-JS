14fa188c978ffd8b8ccc4ab793102e4bdafe5584
Riya Raikwar <riyaraikwar60@gmail.com>
2026-09-11 06:07:49 +0000

    code

---
+++ basics1/code.js
@@ -0,0 +1,17 @@
+// function a() {
+//     count = 0;
+//     function b() {
+//         count++;
+//         console.log(count);
+        
+//     }
+//     return b;
+// }
+// let counter = a();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+
