5681ee975b4daa9d6b0fdf71b273e801120e0d08
Riya Raikwar <riyaraikwar60@gmail.com>
2026-09-16 20:39:48 +0000

    updates

--- basics1/callback.js
+++ basics1/callback.js
@@ -1,27 +1,38 @@
 
-setTimeout(() => {
-    console.log("timer");
+// setTimeout(() => {
+//     console.log("timer");
     
-}, 5000);
+// }, 5000);
 
 
 
 
 
-function x(y) {
-    console.log("function x");
-    y();
-}
+// function x(y) {
+//     console.log("function x");
+//     y();
+// }
 
-x(function y() {
-    console.log("function y");
+// x(function y() {
+//     console.log("function y");
     
     
-}
+// }
 
 
-)
+// )
 
 
 // javascript just have 1 call stack and you can call it a main thread. everythingg that is executing in the web is through call  stack only
 // instead of blocking main thread (call stack) we use async operation
+
+console.log("A");
+
+setTimeout(() => {
+    console.log("B");
+}, 5000);
+
+console.log("C");
+
+// So asynchronous programming allows JavaScript 
+// to start a time-consuming operation and continue executing other code instead of blocking everything.
