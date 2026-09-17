3b422cf163a6767f03fc044fade2605def734b0e
Riya Raikwar <riyaraikwar60@gmail.com>
2026-09-17 19:57:56 +0000

    updates

--- basics1/callback.js
+++ basics1/callback.js
@@ -26,13 +26,27 @@
 // javascript just have 1 call stack and you can call it a main thread. everythingg that is executing in the web is through call  stack only
 // instead of blocking main thread (call stack) we use async operation
 
-console.log("A");
+// console.log("A");
 
-setTimeout(() => {
-    console.log("B");
-}, 5000);
+// setTimeout(() => {
+//     console.log("B");
+// }, 5000);
 
-console.log("C");
+// console.log("C");
 
 // So asynchronous programming allows JavaScript 
 // to start a time-consuming operation and continue executing other code instead of blocking everything.
+
+function attacheventlistner(){
+        let count = 0;
+document.getElementById("clickme").addEventListener("click",function xyz(){
+  console.log("Button clicked", ++count);
+
+});
+
+}
+attacheventlistner()
+
+//Event listener are heavy required memory as even if the call stack is empty if wont free up the space of variable memory
+// system will scope 
+// garabage collection and removeeventlistener is must 
